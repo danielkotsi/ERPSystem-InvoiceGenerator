e40c0cd4cc434bda33de4bc4df0dde98235bbc01
Super User <root@172.16.10.246>
2025-12-16 16:29:11 +0000

    just saving

--- internal/backend/services/invoices_service.go
+++ internal/backend/services/invoices_service.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"-invoice_manager/internal/backend/models"
 	"-invoice_manager/internal/backend/repos"
-	"-invoice_manager/internal/utils"
-	"fmt"
+	"log"
+	// "-invoice_manager/internal/utils"
+	"encoding/json"
 	"net/http"
+	"os"
 )
 
 type InvoiceService struct {
@@ -23,22 +25,34 @@ func NewInvoiceService(in repository.Invoice_repo, mydata repository.MyData_repo
 
 func (s *InvoiceService) CreateInvoice(ctx context.Context, r *http.Request) (pdf []byte, err error) {
 	var invo models.Invoice
-	err = utils.ParseFormData(r, &invo)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println("hello this is the invo from the form", invo)
 
-	err = s.Invoice.CompleteInvoice(ctx, &invo)
+	file, err := os.Open("../../finalinvoiceexample.pretty.json")
 	if err != nil {
 		return nil, err
 	}
-	err = s.MyData.SendInvoice(ctx, &invo)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println("\n\n\nthis is the invo QrCodeURL \n\n", invo.QrURL)
+	defer file.Close()
 
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&invo)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// err = utils.ParseFormData(r, &invo)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// fmt.Println("hello this is the invo from the form", invo)
+	//
+	// err = s.Invoice.CompleteInvoice(ctx, &invo)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// err = s.MyData.SendInvoice(ctx, &invo)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// fmt.Println("\n\n\nthis is the invo QrCodeURL \n\n", invo.QrURL)
 	pdf, err = s.Invoice.MakePDF(ctx, &invo)
 	if err != nil {
 		return nil, err
